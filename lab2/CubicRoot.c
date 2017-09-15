d0ac8616bf55c013725e934664f78034633e0a43
olivierldff <olivier.ldff@gmail.com>
2017-09-15 08:50:22 +0200

    a

--- lab2/CubicRoot.c
+++ lab2/CubicRoot.c
@@ -3,11 +3,22 @@
  
 int main() 
 {
-	double number, root;
+	int iter, i;
+	double number, approx;
 	printf("number?\n");
 	scanf("%lf", &number);
-	printf("Root is %lf\n", root = sqrt(number));
-	printf("approximation is %lf\n", ((number / pow(root, 2)) + (2*root))/3);
+	printf("approx?\n");
+	scanf("%lf", &approx);
+	printf("iter?\n");
+	scanf("%d", &iter);
+
+	for(i = 0; i <= iter; ++i)
+	{
+		approx = (number/(pow(approx,2))+2*approx)/3;
+	}
+
+	//printf("Root is %lf\n", root = sqrt(number));
+	printf("approximation is %lf\n", approx);
 
 #if WIN32
 	system("pause");
